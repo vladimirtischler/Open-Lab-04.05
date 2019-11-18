e9ac19d400c690caa9d8b8305388d073273f37a8
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-18 20:43:38 +0100

    vlado

---
+++ Open-Lab-04.05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Open_Lab_04._02
+{
+    class Program
+    {
+        public static string DoubleChar(string original, int b)
+        {
+            string a = "";
+            for (int i = 0; i < original.Length; i++)
+            {
+                for (int c = 0; c < b; c++)
+                {
+                    a = a + original[i];
+                }
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter count repetition!");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter word!");
+            string word = Console.ReadLine();
+            Console.WriteLine(DoubleChar(word, count));
+        }
+    }
+}
